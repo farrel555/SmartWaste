7488fb3b6cbf555102f4bea34b6361857729ac0c
farrel555 <azrelazmi2@gmail.com>
2025-06-11 11:06:15 +0700

    'tampilan'

--- src/scripts/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  BASE_URL: 'https://story-api.dicoding.dev/v1', // URL API Story
-  MAP_API_KEY: 'Du4PLKxSUoYiUTN8u8bl', // API Key untuk peta digital
-};
-
-export default CONFIG;
